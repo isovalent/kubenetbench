35c35909f1c92b1eff2929a8780199984340bd27
Kornilios Kourtis <kornilios@isovalent.com>
2020-08-04 21:03:53 +0200

    np-rr.sh: misc
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- scripts/np-rr.sh
+++ scripts/np-rr.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # wrapper script for netperf, intended to be used for RR benchmarks
 
+set -e
+
 # queue depth: packets in flight
 QD=${QD:-"1 2 4 8 16 32 64 128"}
 
 # packet (req/res) sizes
 RR_SIZES=${RR_SIZES:-"1,1 1,1024, 1024,1"}
 
-RR_TESTS="tcp_rr udp_rr tcp_crr "
+RR_TESTS="tcp_rr udp_rr tcp_crr"
 
 ARGS=()
 TEST_ARGS=()
@@ -37,8 +39,8 @@ done
 for rrt in ${RR_TESTS}; do
     case $rrt in
         tcp_rr|udp_rr)
-            for qd in ${QD}; do
-                for rr_size in ${RR_SIZES}; do
+            for rr_size in ${RR_SIZES}; do
+                for qd in ${QD}; do
                     echo netperf ${ARGS[@]} -t $rrt -- ${TEST_ARGS[@]} -r $rr_size -b $qd
                     if [ -z "$DRY_RUN" ]; then
                          netperf ${ARGS[@]} -t $rrt -- ${TEST_ARGS[@]} -r $rr_size -b $qd
