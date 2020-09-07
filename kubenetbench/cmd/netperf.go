886ea971e3075310f79a47a6eb743c0a2ff49eba
Kornilios Kourtis <kornilios@isovalent.com>
2020-09-07 11:41:23 +0200

    netperf tcp_stream support
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

--- kubenetbench/cmd/netperf.go
+++ kubenetbench/cmd/netperf.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/spf13/cobra"
 
 	"github.com/kkourt/kubenetbench/kubenetbench/core"
@@ -13,32 +16,45 @@ var netperfBenchArgs []string
 func init() {
 }
 
+var netperfBenchMap = map[string]func() core.Benchmark{
+	"tcp_rr": func() core.Benchmark {
+		cnf := core.NetperfRRConf{core.NetperfConfDefault("tcp_rr", netperfArgs, netperfBenchArgs)}
+		cnf.Timeout = benchmarkDuration
+		return &cnf
+	},
+
+	"tcp_crr": func() core.Benchmark {
+		cnf := core.NetperfRRConf{core.NetperfConfDefault("tcp_crr", netperfArgs, netperfBenchArgs)}
+		cnf.Timeout = benchmarkDuration
+		return &cnf
+	},
+
+	"tcp_stream": func() core.Benchmark {
+		cnf := core.NetperfStreamConf{core.NetperfConfDefault("tcp_stream", netperfArgs, netperfBenchArgs)}
+		cnf.Timeout = benchmarkDuration
+		return &cnf
+	},
+}
+
 func addNetperfFlags(cmd *cobra.Command) {
 	// TODO: add some validation here
-	cmd.Flags().StringVar(&netperfTy, "netperf-type", "tcp_rr", "netperf type benchmark (tcp_rr, tcp_crr, script-np-rr)")
+	tys := make([]string, 0, len(netperfBenchMap))
+	for ty := range netperfBenchMap {
+		tys = append(tys, ty)
+	}
+
+	tyHelp := fmt.Sprintf("netperf type benchmark (available values: %s)", strings.Join(tys, ","))
+	cmd.Flags().StringVar(&netperfTy, "netperf-type", "tcp_rr", tyHelp)
 	cmd.Flags().StringArrayVar(&netperfArgs, "netperf-args", []string{}, "netperf arguments")
 	cmd.Flags().StringArrayVar(&netperfBenchArgs, "netperf-bench-args", []string{}, "netperf benchmark arguments (after --)")
 }
 
 // TODO: parse options to support other netperf configurations here
 func getNetperfBench() core.Benchmark {
-	switch netperfTy {
-	case "tcp_rr":
-		cnf := core.NetperfRRConf{core.NetperfConfDefault("tcp_rr", netperfArgs, netperfBenchArgs)}
-		cnf.Timeout = benchmarkDuration
-		return &cnf
 
-	case "tcp_crr":
-		cnf := core.NetperfRRConf{core.NetperfConfDefault("tcp_crr", netperfArgs, netperfBenchArgs)}
-		cnf.Timeout = benchmarkDuration
-		return &cnf
-
-	case "script-np-rr":
-		cnf := core.NetperfRRConf{core.NetperfConfDefault("tcp_rr,udp_rr,tcp_crr", netperfArgs, netperfBenchArgs)}
-		cnf.Timeout = benchmarkDuration
-		cnf.CliCommand = "scripts/np-rr.sh"
-		return &cnf
+	initFn, ok := netperfBenchMap[netperfTy]
+	if !ok {
+		panic(fmt.Sprintf("Invalid netperf type: %s", netperfTy))
 	}
-
-	panic("Fail!")
+	return initFn()
 }
--- kubenetbench/core/netperf.go
+++ kubenetbench/core/netperf.go
@@ -29,11 +29,6 @@ func NetperfConfDefault(testname string, args []string, benchArgs []string) Netp
 	}
 }
 
-// NetperfRRConf RR netperf configuration
-type NetperfRRConf struct {
-	NetperfConf
-}
-
 // GetTimeout returns the benchmark timeout
 func (cnf *NetperfConf) GetTimeout() int {
 	return cnf.Timeout
@@ -51,6 +46,27 @@ func (cnf *NetperfConf) WriteSrvContainerYaml(pw *utils.PrefixWriter, params map
 	pw.AppendNewLineOrDie(`]`)
 }
 
+// WriteSrvPortsYaml writes the ports part of yaml (e.g., for services)
+func (cnf *NetperfConf) WriteSrvPortsYaml(pw *utils.PrefixWriter, params map[string]interface{}) {
+	pw.AppendNewLineOrDie(`- name: netperf-ctl`)
+	pw.AppendNewLineOrDie(`  protocol: TCP`)
+	pw.AppendNewLineOrDie(`  port: 12865`)
+	pw.AppendNewLineOrDie(`  targetPort: 12865`)
+	pw.AppendNewLineOrDie(`- name: netperf-data`)
+	pw.AppendNewLineOrDie(`  protocol: TCP`)
+	pw.AppendNewLineOrDie(fmt.Sprintf(`  port: %d`, cnf.DataPort))
+	pw.AppendNewLineOrDie(fmt.Sprintf(`  targetPort: %d`, cnf.DataPort))
+}
+
+/**
+ * RR
+ */
+
+// NetperfRRConf RR netperf configuration
+type NetperfRRConf struct {
+	NetperfConf
+}
+
 // Additional options, we might want to add
 // -I <optionspec>
 //     This option enables the calculation of confidence intervals and sets the
@@ -61,15 +77,50 @@ func (cnf *NetperfConf) WriteSrvContainerYaml(pw *utils.PrefixWriter, params map
 // -T <optionspec>
 //       This option controls the CPU, and probably by extension memory, affinity of netperf and/or netserver.
 
+func netperfOutFieldsCommon() []string {
+	return []string{
+		"THROUGHPUT",
+		"THROUGHPUT_UNITS",
+		"THROUGHPUT_CONFID",
+		"PROTOCOL",
+		"ELAPSED_TIME",
+		//
+		"LOCAL_SEND_CALLS",
+		"LOCAL_BYTES_PER_SEND",
+		"LOCAL_RECV_CALLS",
+		"LOCAL_BYTES_PER_RECV",
+		"REMOTE_SEND_CALLS",
+		"REMOTE_BYTES_PER_SEND",
+		"REMOTE_RECV_CALLS",
+		"REMOTE_BYTES_PER_RECV",
+		//
+		"LOCAL_SYSNAME",
+		"LOCAL_RELEASE",
+		"LOCAL_VERSION",
+		"LOCAL_MACHINE",
+		"REMOTEL_SYSNAME",
+		"REMOTEL_RELEASE",
+		"REMOTEL_VERSION",
+		"REMOTEL_MACHINE",
+		//
+		"COMMAND_LINE",
+		// "DIRECTION",
+		// "LOCAL_CPU_BIND",
+		// "REMOTE_CPU_BIND",
+		"LOCAL_TRANSPORT_RETRANS",
+		"REMOTE_TRANSPORT_RETRANS",
+	}
+}
+
 // WriteCliContainerYaml writes the client yaml
 func (cnf *NetperfRRConf) WriteCliContainerYaml(pw *utils.PrefixWriter, params map[string]interface{}) {
 	serverIP, ok := params["serverIP"]
 	if !ok {
 		panic("serverIP undefined")
 	}
-	outputFields := []string{
-		"THROUGHPUT",
-		"THROUGHPUT_UNITS",
+
+	outputFields := append(
+		netperfOutFieldsCommon(),
 		"TRANSACTION_RATE",
 		"P50_LATENCY",
 		"P90_LATENCY",
@@ -78,6 +129,84 @@ func (cnf *NetperfRRConf) WriteCliContainerYaml(pw *utils.PrefixWriter, params m
 		"STDEV_LATENCY",
 		"REQUEST_SIZE",
 		"RESPONSE_SIZE",
+		"BURST_SIZE",
+	)
+
+	pw.AppendNewLineOrDie(`name: netperf-cli`)
+	pw.AppendNewLineOrDie(`image: kkourt/kubenetbench`)
+	pw.AppendNewLineOrDie(fmt.Sprintf(`command: ["%s"]`, cnf.CliCommand))
+	pw.AppendNewLineOrDie(`args : [`)
+	pw.PushPrefix("    ")
+	pw.AppendNewLineOrDie(fmt.Sprintf(`"-l", "%d", # timeout`, cnf.Timeout))
+	pw.AppendNewLineOrDie(`"-j", # enable additional statistics`)
+	pw.AppendNewLineOrDie(fmt.Sprintf(`"-H", "%v",`, serverIP))
+	pw.AppendNewLineOrDie(fmt.Sprintf(`"-t", "%s", # testname`, cnf.TestName))
+	if len(cnf.MoreArgs) > 0 {
+		pw.AppendNewLineOrDie("# Additional args")
+		for _, arg := range cnf.MoreArgs {
+			pw.AppendNewLineOrDie(fmt.Sprintf(`"%s",`, arg))
+		}
+	}
+
+	pw.AppendNewLineOrDie(`"--",`)
+	pw.AppendNewLineOrDie("# Benchmark args")
+	pw.AppendNewLineOrDie(fmt.Sprintf(`"-P", ",%d", # data connection port`, cnf.DataPort))
+	// -D seems to kill the performance for high queue depths, so don't use it
+	// pw.AppendNewLineOrDie(`"-D",# no delay`)
+	pw.AppendNewLineOrDie(fmt.Sprintf(`"-k", "%s",`, strings.Join(outputFields, ",")))
+	if len(cnf.MoreBenchArgs) > 0 {
+		pw.AppendNewLineOrDie("# Additional test-specific args")
+		for _, arg := range cnf.MoreBenchArgs {
+			pw.AppendNewLineOrDie(fmt.Sprintf(`"%s",`, arg))
+		}
+	}
+	pw.PopPrefix()
+	pw.AppendNewLineOrDie(`]`)
+}
+
+/**
+ * STREAM
+ */
+
+// NetperfStreamConf STREAM netperf configuration
+type NetperfStreamConf struct {
+	NetperfConf
+}
+
+// WriteCliContainerYaml writes the client yaml
+func (cnf *NetperfStreamConf) WriteCliContainerYaml(pw *utils.PrefixWriter, params map[string]interface{}) {
+	serverIP, ok := params["serverIP"]
+	if !ok {
+		panic("serverIP undefined")
+	}
+	outputFields := []string{
+		"THROUGHPUT",
+		"THROUGHPUT_UNITS",
+		"THROUGHPUT_CONFID",
+		"LOCAL_SEND_SIZE",
+		"LOCAL_RECV_SIZE",
+		"REMOTE_SEND_SIZE",
+		"REMOTE_RECV_SIZE",
+		"PROTOCOL",
+		"LOCAL_SEND_CALLS",
+		"LOCAL_BYTES_PER_SEND",
+		"LOCAL_RECV_CALLS",
+		"LOCAL_BYTES_PER_RECV",
+		"REMOTE_SEND_CALLS",
+		"REMOTE_BYTES_PER_SEND",
+		"REMOTE_RECV_CALLS",
+		"REMOTE_BYTES_PER_RECV",
+		//
+		"LOCAL_SYSNAME",
+		"LOCAL_RELEASE",
+		"LOCAL_VERSION",
+		"LOCAL_MACHINE",
+		"REMOTEL_SYSNAME",
+		"REMOTEL_RELEASE",
+		"REMOTEL_VERSION",
+		"REMOTEL_MACHINE",
+		//
+		"COMMAND_LINE",
 		// "DIRECTION",
 		// "LOCAL_CPU_BIND",
 		// "REMOTE_CPU_BIND",
@@ -114,16 +243,4 @@ func (cnf *NetperfRRConf) WriteCliContainerYaml(pw *utils.PrefixWriter, params m
 	}
 	pw.PopPrefix()
 	pw.AppendNewLineOrDie(`]`)
-}
-
-// WriteSrvPortsYaml writes the ports part of yaml (e.g., for services)
-func (cnf *NetperfConf) WriteSrvPortsYaml(pw *utils.PrefixWriter, params map[string]interface{}) {
-	pw.AppendNewLineOrDie(`- name: netperf-ctl`)
-	pw.AppendNewLineOrDie(`  protocol: TCP`)
-	pw.AppendNewLineOrDie(`  port: 12865`)
-	pw.AppendNewLineOrDie(`  targetPort: 12865`)
-	pw.AppendNewLineOrDie(`- name: netperf-data`)
-	pw.AppendNewLineOrDie(`  protocol: TCP`)
-	pw.AppendNewLineOrDie(fmt.Sprintf(`  port: %d`, cnf.DataPort))
-	pw.AppendNewLineOrDie(fmt.Sprintf(`  targetPort: %d`, cnf.DataPort))
 }
